7e673766f9734fad3538f00db1ee63f1ec080ef3
RodionovNikita <radzivonaumikita@gmail.com>
2024-06-10 20:59:49 +0200

    seminar 5 homework added

---
+++ src/main/java/ru/gb/seminar_5/HW/Fork.java
@@ -0,0 +1,13 @@
+package ru.gb.seminar_5.HW;
+
+public class Fork {
+    private boolean using;
+
+    public boolean isUsing() {
+        return using;
+    }
+
+    public void setUsing(boolean using) {
+        this.using = using;
+    }
+}
---
+++ src/main/java/ru/gb/seminar_5/HW/Main.java
@@ -0,0 +1,8 @@
+package ru.gb.seminar_5.HW;
+
+public class Main {
+    public static void main(String[] args) {
+        Table table = new Table();
+        table.start();
+    }
+}
